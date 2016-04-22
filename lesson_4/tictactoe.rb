1228164dc937edc8826481dab292c53e9073ef44
ttscott2 <timothytscott3@gmail.com>
2016-04-21 20:41:24 -0500

    add main gain loop

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -8,6 +8,7 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -44,9 +45,25 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
+end
+
 board = initialize_board
 display_board(board)
 
-player_places_piece!(board)
-puts board.inspect
-display_board(board)
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
